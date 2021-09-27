dd2ee4228dc0dab5d6c1c96e586ab65b5738f7bf
Nick <nwestallen@gmail.com>
2021-09-27 15:53:15 -0400

    creates Animal and Vehicle interfaces

---
+++ exampleTransport/src/transport/Animal.java
@@ -0,0 +1,15 @@
+package transport;
+
+// Interface - defines what methods a child class can do
+
+// Animal (Parent, Super Class)
+// turtle (child class)
+// horse (child class)
+// rabbit (child class)
+
+public interface Animal {
+    
+    void move();
+    void eat(int i);
+    String speak();    
+}
